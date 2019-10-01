fbcde5f09a1fa8041decc943ad9717018236b835
Callum <calluml@rd008520.rd.bbc.co.uk>
2019-10-01 14:09:20 +0100

    chips

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const rtm = new RTMClient(token, { agent: new HttpsProxyAgent(proxyUrl)  });
     const { self, team  } = await rtm.start();
 })();
 
-
+var LunchTimeDeclared = false;
+var LunchTime = {
+    'h': 12,
+    'm': 30
+};
 
 rtm.on('message', (event) => {
 //    console.log(event);
@@ -28,6 +32,9 @@ rtm.on('message', (event) => {
             switch(MsgArgs[1]){
                 case "chips":
                     chipsHandler(MsgArgs, event);
+                    break;
+                case "lunchtime":
+                    lunchtimeHandler(MsgArgs, event);
                     break;
                 default:
                     rtm.sendMessage("I don't recognise the input \"".concat(MsgArgs[1], "\". Please have some chips and then try again."), event.channel)
@@ -58,4 +65,40 @@ function chipsHandler(MsgArgs, event){
         default:
             return;
     }
+}
+
+const TimeRegExp = /\d\d:\d\d/;
+
+function lunchtimeHandler(MsgArgs, event){
+    switch(MsgArgs[2]){
+        case "set":
+        case "change":
+            var RightForm = TimeRegExp.test(MsgArgs[3]);
+            if(RightForm){
+                setLunchtime(MsgArgs[3],(MsgArgs[2] == "set" ? "s" : "c"), event.channel);
+            }
+            else{
+                rtm.sendMessage("Please provide 24h time with leading zeroes in the format hh:mm.",event.channel);
+            }
+            break;
+
+
+    }
+}
+
+function setLunchtime(TimeIn, sc, channel){
+    if(LunchTimeDeclared && sc == "s"){
+        rtm.sendMessage("Lunchtime has already been set for today. Please use this command in the form \"lunchtime change <hh:mm>\".",channel);
+        return;
+    }
+    var lth = parseInt(TimeIn.slice(0,2));
+    var ltm = parseInt(TimeIn.slice(3));
+    if(lth > 23 || ltm > 59){
+        rtm.sendMessage("Please enter a valid time.",channel);
+        return;
+    }
+    LunchTime.h = lth;
+    LunchTime.m = ltm;
+    LunchTimeDeclared = true;
+    sc == "s" ? rtm.sendMessage("Lunchtime set to ".concat(TimeIn.slice(0,2),":",TimeIn.slice(3),"."),channel) : rtm.sendMessage("Lunchtime changed to ".concat(TimeIn.slice(0,2),":",TimeIn.slice(3),"."),channel);
 }
